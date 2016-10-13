314eccf3fd20ddf0af5fae0707c61a0d5f91a53f
Koen van Gilst <koen.vangilst@gmail.com>
2016-10-13 20:15:39 +0200

    adds echo of generated file

--- spits.sh
+++ spits.sh
@@ -1,5 +1,5 @@
 mkdir -p  /Users/vnglst/spits/$(date +%Y)/$(date +%m)/$(date +%d) && cd "$_"
-# Recording between 6:00 and 19:00
+# Recording between 4:00 and 20:00
 while [[ "$(date +"%T")" > '04:00:00' ]] && [[ "$(date +"%T")" < '20:00:00' ]]
 do
         # open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,10z/data=!5m1!1e1"
@@ -12,3 +12,4 @@ done
 echo "Done recording."
 echo "Generating video from jpg files"
 convert -quality 100 *.jpg -gravity South -pointsize 80 -annotate +0+100 %f randstad-$(date +%y-%m-%d_%H:%M:%S).mp4
+echo "Generated file: $_"
