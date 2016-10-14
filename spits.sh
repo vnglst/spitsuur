0674c95cd63e7e826ca7af01214dfbb9df18fa52
Koen van Gilst <koen.vangilst@gmail.com>
2016-10-14 16:43:25 +0200

    adds Youtube upload script

--- spits.sh
+++ spits.sh
@@ -3,16 +3,19 @@ mkdir -p screenshots/$(date +%Y)/$(date +%m)/$(date +%d) && cd "$_"
 # Recording between 4:00 and 20:00
 while [[ "$(date +"%T")" > '04:00:00' ]] && [[ "$(date +"%T")" < '20:00:00' ]]
 do
-        # open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,10z/data=!5m1!1e1"
-        open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,9z/data=!5m1!1e1"
-        echo "Loading Google Maps (with traffic) in Safari"
-        sleep 10 # 60 seconds
-        echo "Making screenshot of current traffic"
-        screencapture -S -t jpg $(date +%H:%M:%S).jpg
+		open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,9z/data=!5m1!1e1"
+		echo "Loading Google Maps (with traffic) in Safari"
+		sleep 60 # 60 seconds
+		echo "Making screenshot of current traffic"
+		screencapture -S -t jpg $(date +%H:%M:%S).jpg
 done
 echo "Done recording."
 echo "Generating video from jpg files"
 convert -quality 100 *.jpg \
 		-gravity South -pointsize 80 -annotate +0+100 %f \
-		../../../../videos/randstad-$(date +%y-%m-%d_%H-%M-%S).mp4
-echo "Generated file: $_"
+		randstad-$(date +%y-%m-%d_%H-%M-%S).mp4
+VIDEOFILE=$_
+mv $VIDEOFILE ../../../../videos/
+cd ../../../../
+echo "Uploading video $VIDEOFILE to YouTube"
+node upload.js videos/$VIDEOFILE
