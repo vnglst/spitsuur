f09f07986325c88e0cb17d1491d0c6ec43421a64
Koen van Gilst <koen.vangilst@gmail.com>
2016-10-17 10:21:54 +0200

    updates screenshots folder

--- spits.sh
+++ spits.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 mkdir -p videos
-mkdir -p screenshots/$(date +%Y)/$(date +%m)/$(date +%d) && cd "$_"
+mkdir -p screenshots/$(date +%Y-%m-%d) && cd "$_"
 # Recording between 4:00 and 20:00
-while [[ "$(date +"%T")" > '04:00:00' ]] && [[ "$(date +"%T")" < '20:00:00' ]]
-do
+ while [[ "$(date +"%T")" > '04:00:00' ]] && [[ "$(date +"%T")" < '20:00:00' ]]
+ do
 		open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,9z/data=!5m1!1e1"
 		echo "Loading Google Maps (with traffic) in Safari"
 		sleep 60 # 60 seconds
@@ -15,7 +16,7 @@ convert -delay 10 -quality 100 *.jpg \
 		-gravity South -pointsize 80 -annotate +0+100 %f \
 		randstad-$(date +%y-%m-%d_%H-%M-%S).mp4
 VIDEOFILE=$_
-mv $VIDEOFILE ../../../../videos/
-cd ../../../../
+mv $VIDEOFILE ../../videos/
+cd ../../
 echo "Uploading video $VIDEOFILE to YouTube"
 node upload.js videos/$VIDEOFILE
