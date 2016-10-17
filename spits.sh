8e7be7bd1e8067040e318604ff0475620b35868f
Koen van Gilst <koen.vangilst@gmail.com>
2016-10-17 16:07:12 +0200

    cleanup echos, less verbose output

--- spits.sh
+++ spits.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
+ROOT=`pwd`
 mkdir -p videos
 mkdir -p screenshots/$(date +%Y-%m-%d) && cd "$_"
 # Recording between 4:00 and 20:00
- while [[ "$(date +"%T")" > '04:00:00' ]] && [[ "$(date +"%T")" < '20:00:00' ]]
- do
-		open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,9z/data=!5m1!1e1"
-		echo "Loading Google Maps (with traffic) in Safari"
-		sleep 60 # 60 seconds
-		echo "Making screenshot of current traffic"
-		screencapture -S -t jpg $(date +%H:%M:%S).jpg
-done
-echo "Done recording."
+echo "Loading Google Maps (with traffic) in Safari"
+# while [[ "$(date +"%T")" > '04:00:00' ]] && [[ "$(date +"%T")" < '20:00:00' ]]
+# do
+	open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,9z/data=!5m1!1e1"
+	sleep 60 # 60 seconds
+	screencapture -S -t jpg $(date +%H:%M:%S).jpg
+# done
+echo "Done recording screenshots."
 echo "Generating video from jpg files"
 convert -delay 10 -quality 100 *.jpg \
 		-gravity South -pointsize 80 -annotate +0+100 %f \
-		randstad-$(date +%y-%m-%d_%H-%M-%S).mp4
-VIDEOFILE=$_
-mv $VIDEOFILE ../../videos/
-cd ../../
-echo "Uploading video $VIDEOFILE to YouTube"
-node upload.js videos/$VIDEOFILE
+		$ROOT/videos/randstad-$(date +%y-%m-%d_%H-%M-%S).mp4
+VIDEOFILE="$_"
+cd $ROOT
+echo "Uploading $VIDEOFILE to YouTube"
+node upload.js $VIDEOFILE
+echo "Moving $VIDEOFILE to uploaded folder"
+cd videos
+mkdir -p uploaded
+mv $VIDEOFILE uploaded
+echo "All done"
