5d30ff3150285d650d0d52cb0251bfba47243412
Koen van Gilst <koen.vangilst@gmail.com>
2016-10-17 10:50:26 +0200

    updates git ignore

--- spits.sh
+++ spits.sh
@@ -2,13 +2,13 @@
 mkdir -p videos
 mkdir -p screenshots/$(date +%Y-%m-%d) && cd "$_"
 # Recording between 4:00 and 20:00
- while [[ "$(date +"%T")" > '04:00:00' ]] && [[ "$(date +"%T")" < '20:00:00' ]]
- do
-		open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,9z/data=!5m1!1e1"
-		echo "Loading Google Maps (with traffic) in Safari"
-		sleep 60 # 60 seconds
-		echo "Making screenshot of current traffic"
-		screencapture -S -t jpg $(date +%H:%M:%S).jpg
+while [[ "$(date +"%T")" > '04:00:00' ]] && [[ "$(date +"%T")" < '20:00:00' ]]
+do
+	open -a safari "https://www.google.nl/maps/@52.1404712,4.9350591,9z/data=!5m1!1e1"
+	echo "Loading Google Maps (with traffic) in Safari"
+	sleep 60 # 60 seconds
+	echo "Making screenshot of current traffic"
+	screencapture -S -t jpg $(date +%H:%M:%S).jpg
 done
 echo "Done recording."
 echo "Generating video from jpg files"
