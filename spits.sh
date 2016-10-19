4f34c9dd334a51a5f3c8358be56ec50747f3d97f
Koen van Gilst <koen.vangilst@gmail.com>
2016-10-19 08:50:59 +0200

    moves label to NorthWest

--- spits.sh
+++ spits.sh
@@ -22,7 +22,7 @@ echo "Done recording screenshots."
 echo "Generating video from jpg files"
 convert -delay 10 -quality 100 \
 		$ROOT/$SCREENSHOTS/*.jpg \
-		-gravity South -pointsize 80 -annotate +0+100 %f \
+		-gravity NorthWest -pointsize 80 -annotate +0+100 %f \
 		$ROOT/videos/randstad-$DATE.mp4
 VIDEOFILE="randstad-$DATE.mp4"
 
